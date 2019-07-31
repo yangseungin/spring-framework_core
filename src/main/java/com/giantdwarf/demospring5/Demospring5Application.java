9bbaa96d73518122ff2301c703978e7f8dd9e6ab
seungin yang <rhfpdk92@naver.com>
2019-08-01 08:28:29 +0900

    chap6-2

--- src/main/java/com/giantdwarf/demospring5/Demospring5Application.java
+++ src/main/java/com/giantdwarf/demospring5/Demospring5Application.java
@@ -2,8 +2,10 @@ package com.giantdwarf.demospring5;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource("classpath:/app.properties")
 public class Demospring5Application {
 
     public static void main(String[] args) {
