35abddacfa3fbcedc3d4f0133b9f7eb14912074b
seungin yang <rhfpdk92@naver.com>
2019-08-04 15:39:04 +0900

    Resource 추상화

--- src/main/java/com/giantdwarf/demospring5/AppRunner.java
+++ src/main/java/com/giantdwarf/demospring5/AppRunner.java
@@ -3,8 +3,8 @@ package com.giantdwarf.demospring5;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
+import org.springframework.context.ApplicationContext;
 import org.springframework.core.io.Resource;
-import org.springframework.core.io.ResourceLoader;
 import org.springframework.stereotype.Component;
 
 import java.nio.file.Files;
@@ -14,11 +14,18 @@ import java.nio.file.Path;
 public class AppRunner implements ApplicationRunner {
 
     @Autowired
-    ResourceLoader resourceLoader;
+    ApplicationContext resourceLoader;
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
+        System.out.println(resourceLoader.getClass());
+
+
         Resource resource = resourceLoader.getResource("classpath:test.txt");
+        //Resource resource2 = resourceLoader.getResource("test.txt"); // 스프링 부트기반의 어플리케이션을 작성하는 경우 classpath기준으로 많은 리소스를 씀 (classpath접두어를 사용하는것을 추천)
+
+        System.out.println(resource.getClass());
+
         System.out.println(resource.exists());
         System.out.println(resource.getFile());
         System.out.println(resource.getDescription());
