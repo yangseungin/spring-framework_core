66389bba7834daa5acf5e131e475f965648577be
seungin yang <rhfpdk92@naver.com>
2019-08-04 22:22:11 +0900

    Validation 추상화

---
+++ src/main/java/com/giantdwarf/demospring5/EventValidator.java
@@ -0,0 +1,19 @@
+package com.giantdwarf.demospring5;
+
+import org.springframework.validation.Errors;
+import org.springframework.validation.ValidationUtils;
+import org.springframework.validation.Validator;
+
+public class EventValidator implements Validator {
+
+
+    @Override
+    public boolean supports(Class<?> clazz) {
+        return Event.class.equals(clazz);
+    }
+
+    @Override
+    public void validate(Object target, Errors errors) {
+        ValidationUtils.rejectIfEmptyOrWhitespace(errors,"title","notempty","Empty title is now allowd.");
+    }
+}
