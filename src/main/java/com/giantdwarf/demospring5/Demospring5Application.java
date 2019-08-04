8314f6cac7413b2ed8e7f4b00c7b489eb933ba7c
seungin yang <rhfpdk92@naver.com>
2019-08-04 15:13:07 +0900

    container chap 9

--- src/main/java/com/giantdwarf/demospring5/Demospring5Application.java
+++ src/main/java/com/giantdwarf/demospring5/Demospring5Application.java
@@ -2,10 +2,8 @@ package com.giantdwarf.demospring5;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
-@EnableAsync
 public class Demospring5Application {
 
     public static void main(String[] args) {
